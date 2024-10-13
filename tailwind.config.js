4657dc51d5efe365f5ab005b2addfc695de1d634
yafa jawabreh <217853@ppu.edu.ps>
2024-10-13 19:37:04 +0300

    edit pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sue-ellen': ['Sue Ellen Francisco', 'sans-serif'], // Add your font here
+      }
+    },
   },
   plugins: [],
 }
