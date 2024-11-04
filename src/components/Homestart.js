b631c83545bdf28ad4da2e5381d4ac0b55c531ab
yafa jawabreh <217853@ppu.edu.ps>
2024-11-04 16:07:40 +0200

    update

--- src/components/Homestart.js
+++ src/components/Homestart.js
@@ -3,7 +3,7 @@ function Homestart(){
         <>
         <div className=" grid grid-cols-2 gap-1 max-h-full pt-32 pb-44 sm:my-0 sm:mx-24 md:mx-0">
             <div  class= "Div-1" className="block md:ml-32">
-                <h1 class="" className="text-4xl font-sue-ellen font-medium text-gray-700  text-left">ENJOY YOUR HEALTHY <br></br>DELICIOUS FOOD</h1>
+                <h1 class="" className="lg:text-5xl md:text-4xl sm:text-3xl font-sue-ellen font-medium text-gray-700  text-left">ENJOY YOUR HEALTHY <br></br>DELICIOUS FOOD</h1>
                 <p className="text-gray-500 font-normal  pt-5 text-wrap w-2/3 text-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni numquam deserunt alias dignissimos qui.</p>
                 <button type="button" class="float-left mt-7 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-r-full rounded-bl-3xl
  text-sm px-5 py-2.5  mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Book a Table</button>
